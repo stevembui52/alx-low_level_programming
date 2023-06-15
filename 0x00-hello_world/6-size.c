a4d73dfb054aec276c25ae268339e48ebe0f9aa8
stevembui52 <stevembui52@gmail.com>
2023-06-15 05:58:53 -0700

    darlng

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,5 +1,10 @@
-#!/bin/bash
 #include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
 int main(void)
 {
 	printf("size of char: %d byte(s)\n", sizeof(char));
