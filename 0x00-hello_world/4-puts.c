2d5eef5798845ef070d8ee0c7ce7a8125dd18be8
stevembui52 <stevembui52@gmail.com>
2023-06-15 05:35:17 -0700

    hard

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 /**
  *
- * main -  C program that prints exactly Programming is like building a multilingual puzzle, followed by a new line.
- *  
+ * main -  C program that prints exactly Programming.
+ *
  * return: 0 on (success)
  */
-int main (void)
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return 0;
+	return(0);
 }
