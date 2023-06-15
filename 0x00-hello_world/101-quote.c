4695bda557e86fcdbbb1aa1b2e08503dd0fe16d1
stevembui52 <stevembui52@gmail.com>
2023-06-15 06:39:03 -0700

    m

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,7 +6,6 @@
  */
 int main(void)
 {
-	fprintf(stderr, "and that piece of art is useful\" - Dora Kor
-par, 2015-10-1\n", stdout);
+	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-1\n");
         return (1);
 }
