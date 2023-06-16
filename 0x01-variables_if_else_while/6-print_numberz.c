49c792ff891382d0f7c173ec75fbaa6b9c115350
stevembui52 <stevembui52@gmail.com>
2023-06-16 11:35:18 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+        int bus;
+
+        for (bus = 48; bus <= 57; bus++)
+        {
+                putchar(bus);
+        }
+        putchar ('\n');
+        return (0);
+}
