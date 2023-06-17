986bfa60fb59d25462d83a05ed638df959689325
stevembui52 <stevembui52@gmail.com>
2023-06-16 23:40:29 -0700

    101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -10,18 +10,18 @@ int main(void)
 	int b;
 	int c;
 
-	for(a = 48; a <= 55; a++)
+	for (a = 48; a <= 55; a++)
 	{
 		for (b = 49; b <= 56; b++)
 		{
-			for (c =50; c <= 57; c++)
+			for (c = 50; c <= 57; c++)
 			{
 				if (b > a && c > b)
 				{
 					putchar(a);
 					putchar(b);
 					putchar(c);
-					if (a != 57 || b != 56 || c != 55)
+					if (a != 55 || b != 56)
 					{
 						putchar(',');
 						putchar(' ');
