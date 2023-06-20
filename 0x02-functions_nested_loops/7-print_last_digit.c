aab4bd9e67bfb67ca031da648c92e16434a96c8b
stevembui52 <stevembui52@gmail.com>
2023-06-20 09:57:14 -0700

    7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -10,11 +10,16 @@ int print_last_digit(int ml)
 {
 	int last;
 
-	last = ml % 10;
+	last= ml % 10;
 	if (last < 0)
 	{
 		last = last * -1;
+		_putchar(last + '0');
+		return (last);
 	}
-	_putchar(last + '0');
-	return (last);
+	else 
+	{
+                _putchar(last + '0');
+                return (last);
+	}
 }
