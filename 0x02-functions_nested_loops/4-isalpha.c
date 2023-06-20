f7753bae4aa30a0e4e720d5882dd3658b24a904a
stevembui52 <stevembui52@gmail.com>
2023-06-20 07:10:17 -0700

    pi

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * int _isalpha - check alphabet 
+ * __isalpha - check alphabet
  *
  *@c:the character in ASCII code
  *
@@ -8,7 +8,7 @@
  */
 int _isalpha(int c)
 {
-	if ((c >=97 && c >= 122) || (c >= 65 && c  <= 90))
+	if ((c >= 97 && c <= 122) || (c >= 65 && c  <= 90))
 	{
 		return (1);
 	}
