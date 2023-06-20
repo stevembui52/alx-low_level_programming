d19f43fc738b565ceeabb96b6c13fc2d05083816
stevembui52 <stevembui52@gmail.com>
2023-06-20 10:01:42 -0700

    7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -13,9 +13,8 @@ int print_last_digit(int n)
 	last= n % 10;
 	if (last < 0)
 	{
-		last = last * -1;
-		_putchar(last + 48);
-		return (last);
+		_putchar(-last + 48);
+		return (-last);
 	}
 	else 
 	{
