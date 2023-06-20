a2da9c92da5f6858b22f4460d5ad599505e40978
stevembui52 <stevembui52@gmail.com>
2023-06-20 04:58:58 -0700

    p

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+void print_alphabet(void)
+{
+	int m;
+
+	for (m = 97; m <= 122; m++)
+	{
+		_putchar(m);
+	}
+	_putchar('\n');
+}
