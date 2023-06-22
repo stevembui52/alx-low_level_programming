6c370ef2d2451efbb5fb7127b7aef28fbe861213
stevembui52 <stevembui52@gmail.com>
2023-06-22 05:58:51 -0700

    popi

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+/**
+ * main - check code
+ *
+ * Return: 0 always on success
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 1; a <= 100; a++)
+	{
+		if ((a % 3 == 0) && (a % 5 == 0))
+		{
+			printf("FizzBuzz ");
+		}
+		else if (a % 5 == 0)
+		{
+			printf("Buzz ");
+		}
+		else if (a % 3 == 0)
+		{
+			printf("Fizz ");
+		}
+		else
+		{
+		 printf("%d ", a);
+		}
+	}
+	putchar('\n');
+}
+
