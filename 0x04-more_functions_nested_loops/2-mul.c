c1d331cadc8e16ae10c55a5d9d12a0a509999f58
stevembui52 <stevembui52@gmail.com>
2023-06-22 01:22:08 -0700

    pro

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * mul - find the poduct of two integers
+ *@a:first number to multiply
+ *@b:second number to multiply
+ * Return: the product of a and b
+ */
+int mul(int a, int b)
+{
+	int res;
+
+	res = a * b;
+	return (res);
+}
