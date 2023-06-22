8b30e1b9581c02c397bb9434ffb9b13dc241fdd4
stevembui52 <stevembui52@gmail.com>
2023-06-22 01:56:42 -0700

    pi

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_numbers - prints digits 0 to 9
+ *
+ * Return: Always 0.
+ */
+void print_numbers(void)
+{
+	int n;
+
+	for (n = 48; n <= 57; n++)
+	{
+		_putchar(n);
+	}
+	_putchar('\n');
+}
