26ea4855799fbab915f3373efb0370d8548700ec
stevembui52 <stevembui52@gmail.com>
2023-07-03 05:10:36 -0700

    3-strspn.c

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,36 @@
+#include "main.h"
+#include <string.h>
+#include <stdbool.h>
+/**
+ * _strspn -  gets the length of a prefix substring
+ * @s:
+ * @accept:
+ * Return:
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int init, i, j;
+	unsigned int l1 = strlen(s);
+	unsigned int l2 = strlen(accept);
+	
+	for (i = 0; i < l1; i++)
+	{
+		bool fm = false;
+		for (j = 0; j < l2; j++)
+		{
+			if (accept[j] == s[i])
+			{
+				fm = true;
+				break;
+			}
+		}
+		if (fm == false)
+		{
+			break;
+		}
+		else
+			init++;
+	}
+	return (i);
+}
+	
