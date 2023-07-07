34dc96d8bb22f7627ceecd974eee472c5f3fde2a
stevembui52 <stevembui52@gmail.com>
2023-07-07 02:32:11 -0700

    changes name

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -3,10 +3,10 @@
 /**
  *main - prints its name, followed by a new line
  *@argc:argument count
- *@agrv:argument vector
- *return:0 always on success
+ *@argv:argument vector
+ *Return:0 always on success
  */
-int main (int argc, char *argv[])
+int main(int argc, char *argv[])
 {
 	(void) argc;
 	printf("%s\n", argv[0]);
