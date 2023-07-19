bd461ad8982a119c52fa02abde856d41c1d0fab2
stevembui52 <stevembui52@gmail.com>
2023-07-19 02:24:42 -0700

    por

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -18,14 +18,16 @@ int int_index(int *array, int size, int (*cmp)(int))
 	if (array && cmp)
 	{
 		if (size <= 0)
+		{
 			return (-1);
+		}
 		for (i = 0; i < size; i++)
+		{
+			if (cmp(array[i]) != 0)
 			{
-				if (cmp(array[i]) != 0)
-				{
-					return (i);
-				}
+				return (i);
 			}
+		}
 	}
 	return (-1);
 }
