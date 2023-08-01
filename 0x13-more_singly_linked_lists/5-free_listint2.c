f0a0ad6b2a714a4bc70633dd42e48092e2a26744
stevembui52 <stevembui52@gmail.com>
2023-08-01 01:55:53 -0700

    fee2

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - frees a linked list
+ * @head: head of a list.
+ *
+ * Return: no return.
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *tmp;
+	listint_t *curr;
+
+	if (head != NULL)
+	{
+		curr = *head;
+		while ((tmp = curr) != NULL)
+		{
+			curr = curr->next;
+			free(tmp);
+		}
+		*head = NULL;
+	}
+
