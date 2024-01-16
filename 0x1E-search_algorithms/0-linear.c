7c38d592e1d3a2cc85429711e6db5751e72b1e46
stevembui52 <stevembui52@gmail.com>
2024-01-15 22:12:48 -0800

    linear search

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -17,7 +17,7 @@ int linear_search(int *array, size_t size, int value)
 	while (index < size)
 	{
 		printf("Value checked array[%ld] = %d\n", index, array[index]);
-		if(array[index] == value)
+		if (array[index] == value)
 		{
 			return (index);
 		}
